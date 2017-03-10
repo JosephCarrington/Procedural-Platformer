c6487146425d448fc3c41ceab0620c9a010d1135
Joseph Carrington <joseph.carrington@gmail.com>
2017-03-10 16:17:15 -0600

    Added level cam

---
+++ Assets/LevelMapController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMapController : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject player;
+	void Start () {
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = gameObject.transform.position;
+		newPos.x = player.transform.position.x;
+		newPos.y = player.transform.position.y;
+		gameObject.transform.position = newPos;
+	}
+}
