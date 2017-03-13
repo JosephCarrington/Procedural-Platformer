5e4f58948284ca440e5bae5c9d994c56ed688692
Joseph Carrington <joseph.carrington@gmail.com>
2017-03-13 17:24:54 -0500

    Tried and failed to animate map transition

--- Assets/LevelMapController.cs
+++ Assets/LevelMapController.cs
@@ -22,14 +22,31 @@ public class LevelMapController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Map")) {
-			print ("maximizing map");
 			gameObject.GetComponent<tk2dCamera> ().CameraSettings.rect = fullscreenViewportRect;
 			gameObject.GetComponent<tk2dCamera> ().ZoomFactor = fullScreenZoomFactor;
+//			StartCoroutine(MaximizeMap());
 		}
 		if (Input.GetButtonUp ("Map")) {
-			print ("minimizing map");
 			gameObject.GetComponent<tk2dCamera> ().CameraSettings.rect = startingViewportRect;
 			gameObject.GetComponent<tk2dCamera> ().ZoomFactor = startingZoomFactor;
 		}
 	}
+//	float wVel, hVel, xVel, yVel, zoomVel;
+//	public float smoothTime = 0.5f;
+//
+//	IEnumerator MaximizeMap() {
+//		Rect currentRect = gameObject.GetComponent<tk2dCamera> ().CameraSettings.rect;
+//		float currentZoom = gameObject.GetComponent<tk2dCamera> ().ZoomFactor;
+//
+//		while (currentRect != fullscreenViewportRect && currentZoom != fullScreenZoomFactor) {
+//			float newW = Mathf.SmoothDamp (currentRect.width, fullscreenViewportRect.width, ref wVel, smoothTime);
+//			float newH = Mathf.SmoothDamp (currentRect.height, fullscreenViewportRect.height, ref hVel, smoothTime);
+//			float newX = Mathf.SmoothDamp (currentRect.x, fullscreenViewportRect.x, ref xVel, smoothTime);
+//			float newY = Mathf.SmoothDamp (currentRect.y, fullscreenViewportRect.y, ref yVel, smoothTime);
+//			float newZoom = Mathf.SmoothDamp (currentZoom, fullScreenZoomFactor, ref zoomVel, smoothTime);
+//			gameObject.GetComponent<tk2dCamera> ().CameraSettings.rect = new Rect (newX, newY, newW, newH);
+//			gameObject.GetComponent<tk2dCamera> ().ZoomFactor = newZoom;
+//			yield return null;
+//		}
+//	}
 }
