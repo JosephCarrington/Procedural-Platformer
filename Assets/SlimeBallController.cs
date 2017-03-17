4319d65378769f52ea8aae0467a27968846505f7
Joseph Carrington <joseph.carrington@gmail.com>
2017-03-16 19:07:48 -0500

    Changed slimeBall to use trigger for pickup

--- Assets/SlimeBallController.cs
+++ Assets/SlimeBallController.cs
@@ -8,22 +8,15 @@ public class SlimeBallController : MonoBehaviour {
 	void Start () {
 		startTime = Time.time;
 		Physics2D.IgnoreCollision (gameObject.transform.GetComponent<CircleCollider2D> (), GameObject.Find ("Player").GetComponent<BoxCollider2D> ());
-		StartCoroutine (ReallowCollision ());
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
-	IEnumerator ReallowCollision() {
-		yield return new WaitForSeconds (dontCollectTime);
-		Physics2D.IgnoreCollision (gameObject.transform.GetComponent<CircleCollider2D> (), GameObject.Find ("Player").GetComponent<BoxCollider2D> (), false);
-	}
 	public float dontCollectTime = 0.25f;
 	float startTime;
-	void OnCollisionEnter2D(Collision2D col) {
+	void OnTriggerEnter2D(Collider2D col) {
 		if (Time.time < startTime + dontCollectTime) {
 			return;
 		}
