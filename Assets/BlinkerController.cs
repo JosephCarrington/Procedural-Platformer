bb7ce473c3ab5342c95f57f183c367363d2b1e3b
Joseph Carrington <joseph.carrington@gmail.com>
2017-04-24 18:26:22 -0500

    Added instructions scene

---
+++ Assets/BlinkerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class BlinkerController : MonoBehaviour {
+
+	// Use this for initialization
+	bool off = false;
+	void Start () {
+		StartCoroutine (Blink ());
+	}
+
+	void Update() {
+		if (Input.GetButtonDown ("Jump")) {
+			SceneManager.LoadScene ("Instructions");
+		}
+	}
+
+	IEnumerator Blink() {
+		while (true) {
+			yield return new WaitForSeconds (0.25f);
+			if (off) {
+				gameObject.GetComponent<Text>().enabled = false;	
+			} else {
+				gameObject.GetComponent<Text>().enabled = true;	
+			}
+			off = !off;
+		}
+	}
+}
